711383ff22bab4690df0427afe9cffd696677913
Darijan <darijan.isa@iblocks.co.uk>
2013-11-21 17:07:31 +0000

    Test Git Commit

---
+++ test-project/src/main/java/co/uk/iblocks/test/Test.java
@@ -0,0 +1,24 @@
+package co.uk.iblocks.test;
+/**
+ * 
+ */
+
+/**
+ * <p>Provides ...</p>
+ * 
+ * <p>Created on 21 Nov 2013 by isa_d</p>
+ * @author <a href="mailto:darijan.isa@iblocks.co.uk">Darijan Isa</a>
+ */
+/* Copyright (c) 2001-2013 iBlocks Ltd.
+ * 28a, Cole Street, London Bridge, SE1 4YH, England.
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of
+ * iBlocks Ltd. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with iBlocks.
+ */
+public class Test {
+  
+}
